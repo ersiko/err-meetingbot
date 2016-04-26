b2c85a3a2c2f69625fc4fc716d6d3051a4413ba8
Tomas <tomas@criptos.com>
2016-04-26 20:23:29 +0200

    Adding multi-user multi-channel functionality

--- meetingbot.py
+++ meetingbot.py
@@ -15,6 +15,7 @@ __author__ = 'Tomas Nunez'
 
 class MeetingBot(BotPlugin):
 
+
   @botcmd
   def meeting_start(self, mess, args):
     """ Resets all counters to start a meeting and keep track of time
@@ -22,61 +23,93 @@ class MeetingBot(BotPlugin):
     """
     date_today = self.current_date()
     time_now = self.current_time()
-    meetings = self['meetings']
-
+    try:
+        meetings = self['meetings']
+    except KeyError:
+        self['meetings'] = {}
+        meetings = self['meetings']
+    name = self.meetingName(mess)
     if date_today in meetings:
       return "There is already meeting data for today. Do you want to delete it, append to it or create a new meeting for today?"
     else:
-      meetings[date_today] = {time_now: 'Internal'}
+      meetings[name + date_today] = {time_now: 'Internal'}
       self['meetings'] = meetings
       return "Meeting started!"
 
+  @staticmethod
+  def meetingName(mess):
+    if mess.is_group:
+      name = str(mess.to)
+    elif mess.is_direct:
+      name = str(mess.frm)
+    return name       
+           
   @botcmd
   def meeting_end(self, mess, args):
-    """ Ends a meeting
+    """ Tags a meeting as "ended" so no more time can be added to it
     Example: !meeting end
     """
     date_today = self.current_date()
     time_now = self.current_time()
-
+    name = self.meetingName(mess)
     meetings = self['meetings']
-    meetings[date_today][time_now] = "END OF MEETING"
-    self['meetings'] = meetings
-
-  @botcmd
-  def meeting_reset(self, mess, args):
-    """ Delete data from a meeting
-    Example: !meeting reset
-    """
-    date_today = self.current_date()
-    meetings = self['meetings']
-
     try:
-      del meetings[date_today]
-      self['meetings'] = meetings
-      return "Meeting data for day " + date_today + " successfully reset"
+        meetings[name + date_today][time_now] = "END OF MEETING"
+        self['meetings'] = meetings
     except KeyError:
-      return "No meetings today"
+        yield "There is no meeting for this room/user for today. Have you ran \"!meeting start\"?"
+
+#  @botcmd
+#  def meeting_reset(self, mess, args):
+#    """ Delete data from a meeting
+#    Example: !meeting reset
+#    """
+#    date_today = self.current_date()
+#    meetings = self['meetings']
+#
+#    try:
+#      del meetings[date_today]
+#      self['meetings'] = meetings
+#      return "Meeting data for day " + date_today + " successfully reset"
+#    except KeyError:
+#      return "No meetings today"
 
   @botcmd
   def meeting_project(self, mess, args):
+    """ Like stating "We're talking about this project", it adds a record to the meeting, so we keep track of the project time
+    Example: !meeting project foobarproject
+    """
     date_today = self.current_date()
     time_now = self.current_time()
     project = args.strip().title()
-
+    name = self.meetingName(mess)
     meetings = self['meetings']
 
     if project in self['aliases']:
       project = self['aliases'][project]
 
-    yield "Now we started talking about " + project
-    meetings[date_today][time_now] = project
-    self['meetings'] = meetings
+    try: 
+        meetings[name + date_today][time_now] = project
+        yield "Now we started talking about " + project
+        self['meetings'] = meetings
+    except KeyError:
+        yield "There is no meeting for this room/user for today. Have you ran \"!meeting start\"?"
+    return                
+                
 
   @botcmd
   def meeting_times(self, mess, args):
+    """ Once the meeting is finished, this command sorts and adds up all the time spent talking about projects. You need to use "!meeting end" to close the meeting for the times to be accurate.
+    Example: !meeting times
+    """
     date_today = self.current_date()
-    meeting = sorted(self['meetings'][date_today].items())
+    name = self.meetingName(mess)
+    try:
+        meeting = sorted(self['meetings'][name + date_today].items())
+    except KeyError:
+        yield "There is no meeting for this room/user for today. Have you ran \"!meeting start\"?"
+        return
+        
     prev_time = None
     times = {}
     for time, project in meeting:
@@ -100,12 +133,13 @@ class MeetingBot(BotPlugin):
 
   @botcmd
   def meeting_summary(self, mess, args):
-    """ Shows a summary of the time spent on the meeting
-    Example: !meeting summary 2015-7-16
+    """ Shows a summary of the time spent on the meeting, printing all the records
+    Example: !meeting summary
     """
     date_today = self.current_date()
+    name = self.meetingName(mess)
     try:
-      meeting = sorted(self['meetings'][date_today].items())
+      meeting = sorted(self['meetings'][name + date_today].items())
     except KeyError:
       return "No meetings today"
 
@@ -119,23 +153,23 @@ class MeetingBot(BotPlugin):
     """
     return self['meetings'].keys()
 
-  @botcmd
-  def meeting_del(self, mess, args):
-    """ Delete a meeting
-    Example: !meeting delete 2015-7-15
-    """
-    date = args.strip().title()
-    meetings = self['meetings']
-    try:
-      del meetings[date]
-      self['meetings'] = meetings
-      return "Meeting " + date + " deleted successfully"
-    except KeyError:
-      raise "There's no meeting for " + date + " in the database"
+#  @botcmd
+#  def meeting_del(self, mess, args):
+#    """ Delete a meeting
+#    Example: !meeting delete 2015-7-15
+#    """
+#    date = args.strip().title()
+#    meetings = self['meetings']
+#    try:
+#      del meetings[date]
+#      self['meetings'] = meetings
+#      return "Meeting " + date + " deleted successfully"
+#    except KeyError:
+#      raise "There's no meeting for " + date + " in the database"
 
   @botcmd(split_args_with=None)
   def meeting_aliasadd(self, mess, args):
-    """ Assigns an alias to a project name
+    """ Assigns an alias to a project name, so you can use a more convenient name (easy to remember) 
     Example: !meeting addalias Project ProjectAlias
     """
     if len(args) <= 1:
@@ -155,7 +189,7 @@ class MeetingBot(BotPlugin):
 
   @botcmd
   def meeting_aliaslist(self, mess, args):
-    """ Lists all available nicknames
+    """ Lists all available project aliases
     Example: !meeting aliaslist
     """
     return self['aliases']
@@ -187,17 +221,28 @@ class MeetingBot(BotPlugin):
 
   @botcmd
   def time_now(self, mess, args):
+    """ Shows the time right now on the server.
+    Example: !time now
+    """    
     time_format = "%H:%M:%S %Z%z"
     return self.current_time().strftime(time_format)
 
   @botcmd
   def date_today(self, mess, args):
+    """ Shows today's date on the server.
+    Example: !date today
+    """
     return self.current_date()
 
 #  @botcmd
 #  def meeting_init(self, mess, args):
-#    self['meetings'] = {}
+#    del self['meetings'] 
 
 #  @botcmd
 #  def meeting_aliasinit(self, mess, args):
 #    self['aliases'] = {}
+#    return
+    
+#  @botcmd
+#  def meeting_test(self, mess, args):
+#    return (mess.to)
